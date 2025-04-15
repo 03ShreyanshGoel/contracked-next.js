b82a88c622e8d4e269b8dee114c72e47c80bfd02
Shreyansh Goel <03shreyanshgoel@gmail.com>
2025-04-15 10:28:59 +0530

    Update server.ts - 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://contracked.vercel.app/'
+    origin: 'https://contracked.vercel.app'
 }));
 
 app.use(express.json());
